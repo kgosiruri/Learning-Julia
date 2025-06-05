773f15b233d90d8d63cca7dbaf52dd61ed14631d
StochPoisson <55861109+kgosiruri@users.noreply.github.com>
2025-06-05 15:22:14 +0100

    Update Commands.jl

---
+++ Chapter 1/Commands.jl
@@ -0,0 +1,22 @@
+# Commands.jl
+
+# Basic Julia setup and example commands
+
+# Print a greeting
+println("Hello, Julia!")
+
+# Simple arithmetic
+a = 5
+b = 3
+println("a + b = ", a + b)
+
+# Define a function
+function square(x)
+    return x^2
+end
+
+println("The square of 4 is ", square(4))
+
+# Create an array and print it
+arr = [1, 2, 3, 4, 5]
+println("Array: ", arr)
