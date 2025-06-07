50bcd2c06469b1343829c572a2b6f288265ffce5
StochPoisson <55861109+kgosiruri@users.noreply.github.com>
2025-06-07 02:37:03 +0100

    UPDATES

--- Chapter 1/Commands.jl
+++ Chapter 1/Commands.jl
@@ -2,6 +2,7 @@
 
 # Basic Julia setup and example commands
 
+#Hello, World and Perfect Squares
 # Print a greeting
 println("Hello, Julia!")
 
@@ -20,3 +21,4 @@ println("The square of 4 is ", square(4))
 # Create an array and print it
 arr = [1, 2, 3, 4, 5]
 println("Array: ", arr)
+
